b05df95eaf0d7e38a06864249a4a1600dbc57073
Ruan Silva <ruan.fox.2013@gmail.com>
2023-08-17 18:50:22 -0300

    interface

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.png';
+
+function App() {
+  return (
+    <img src={logo}  alt="Tindev" />
+    
+
+
+
+
+
+
+  );
+
+ 
+
+  }
+
+export default App;
