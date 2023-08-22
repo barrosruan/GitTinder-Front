ce410989b0399fcea0b4430bfc65f04edea96e6c
Ruan Silva <ruan.fox.2013@gmail.com>
2023-08-22 20:43:56 -0300

    adicionando navigate

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./Login.css";
 
 import logo from "../assets/logo.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    navigate("/main");
+  }
+
   return (
     <div className="login-contanier">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="Tindev" />
         <h3>GitTinder</h3>
-        <input placeholder="Digite seu usuário do Github" />
+        <input
+          placeholder="Digite seu usuário do Github"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <button type="submit">Enviar</button>
       </form>
     </div>
