1c08d7045ea2f3eb6a182c206f0a2a02873c7b18
Ruan Silva <ruan.fox.2013@gmail.com>
2023-08-25 20:46:58 -0300

    adicionando-style

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import "./Main.css";
 
 import logo from "../assets/logo.png";
 import deslike from "../assets/deslike.png";
@@ -11,6 +12,60 @@ export default function Main() {
     <div className="main-container">
       <img src={logo} alt="GitTinder" />
       <ul>
+        <li>
+          <img
+            src="https://avatars.githubusercontent.com/u/95625134?v=4"
+            alt=""
+          />
+          <footer>
+            <strong>Wollas Romeiro</strong>
+            <p> Programador Backend </p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={deslike} alt="deslike"></img>
+            </button>
+            <button type="button">
+              <img src={like} alt="like"></img>
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src="https://avatars.githubusercontent.com/u/95625134?v=4"
+            alt=""
+          />
+          <footer>
+            <strong>Wollas Romeiro</strong>
+            <p> Programador Backend </p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={deslike} alt="deslike"></img>
+            </button>
+            <button type="button">
+              <img src={like} alt="like"></img>
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src="https://avatars.githubusercontent.com/u/95625134?v=4"
+            alt=""
+          />
+          <footer>
+            <strong>Wollas Romeiro</strong>
+            <p> Programador Backend </p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={deslike} alt="deslike"></img>
+            </button>
+            <button type="button">
+              <img src={like} alt="like"></img>
+            </button>
+          </div>
+        </li>
         <li>
           <img
             src="https://avatars.githubusercontent.com/u/95625134?v=4"
