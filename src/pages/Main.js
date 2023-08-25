2ac02d64d16555561ead1163a951992324cbd892
Ruan Silva <ruan.fox.2013@gmail.com>
2023-08-25 16:01:21 -0300

    implementando pagina

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,35 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import logo from "../assets/logo.png";
+import deslike from "../assets/deslike.png";
+import like from "../assets/like.png";
 
 export default function Main() {
-  return <h1>hello</h1>;
+  let { id } = useParams();
+  return (
+    <div className="main-container">
+      <img src={logo} alt="GitTinder" />
+      <ul>
+        <li>
+          <img
+            src="https://avatars.githubusercontent.com/u/95625134?v=4"
+            alt=""
+          />
+          <footer>
+            <strong>Wollas Romeiro</strong>
+            <p> Programador Backend </p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={deslike} alt="deslike"></img>
+            </button>
+            <button type="button">
+              <img src={like} alt="like"></img>
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
