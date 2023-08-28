2ca88cffb9ef921b1c9067180b0e396fba110567
Ruan Silva <ruan.fox.2013@gmail.com>
2023-08-28 16:30:05 -0300

    implementação-melhorias

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import api from "../services/api";
 import "./Main.css";
 
 import logo from "../assets/logo.png";
@@ -8,82 +9,44 @@ import like from "../assets/like.png";
 
 export default function Main() {
   let { id } = useParams();
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get("/devs/${id}", {
+        headers: {
+          user: id,
+        },
+      });
+
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [id]);
+
   return (
     <div className="main-container">
       <img src={logo} alt="GitTinder" />
       <ul>
-        <li>
-          <img
-            src="https://avatars.githubusercontent.com/u/95625134?v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Wollas Romeiro</strong>
-            <p> Programador Backend </p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={deslike} alt="deslike"></img>
-            </button>
-            <button type="button">
-              <img src={like} alt="like"></img>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars.githubusercontent.com/u/95625134?v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Wollas Romeiro</strong>
-            <p> Programador Backend </p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={deslike} alt="deslike"></img>
-            </button>
-            <button type="button">
-              <img src={like} alt="like"></img>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars.githubusercontent.com/u/95625134?v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Wollas Romeiro</strong>
-            <p> Programador Backend </p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={deslike} alt="deslike"></img>
-            </button>
-            <button type="button">
-              <img src={like} alt="like"></img>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars.githubusercontent.com/u/95625134?v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Wollas Romeiro</strong>
-            <p> Programador Backend </p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={deslike} alt="deslike"></img>
-            </button>
-            <button type="button">
-              <img src={like} alt="like"></img>
-            </button>
-          </div>
-        </li>
+        {users.map((user) => (
+          <li key={user.id}>
+            <img src="{user.avatar}" alt="{user.name}" />
+            <footer>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </footer>
+            <div className="buttons">
+              <button type="button">
+                <img src={deslike} alt="deslike"></img>
+              </button>
+              <button type="button">
+                <img src={like} alt="like"></img>
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
