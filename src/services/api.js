68134512ecebfd817afb271ddb0eb77b81311fbd
Ruan Silva <ruan.fox.2013@gmail.com>
2023-09-28 18:44:21 -0300

    up-cod

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://git-tinder-back-ko3a.vercel.app/",
+  baseURL: "https://git-tinder-back.vercel.app/",
 });
 
 export default api;
